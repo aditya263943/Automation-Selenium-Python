7a49b25610c87c44d81f71e96e394676a666a3f6
Aditya Kumar <50773285+aditya263943@users.noreply.github.com>
2024-11-14 12:40:11 +0530

    Added newfile.py

--- selenium_test.py
+++ selenium_test.py
@@ -8,8 +8,9 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 # Navigate to a webpage (Google homepage as an example)
 driver.get("https://www.novatr.com")
 
-# Print "Hello World" in the console
-print("Hello World")
+# Perform basic interactions like finding an element
+title = "Online Professional Courses for Designers, Architects & Engineers | Novatr"
+print("Title of the page is:", title)
 
 # Close the browser after the operation
 driver.quit()
